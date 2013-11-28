861d0065d27f17691374d817ee0d009ff899667d
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2013-11-28 10:16:47 +0100

    Submodule and configs update

---
+++ dunecontrolparallel.cmd
@@ -0,0 +1,15 @@
+#PBS -o dunecontroloutput.dat
+#PBS -l walltime=00:20:00,nodes=1:westmere:ppn=12
+#PBS -A o0num
+# #PBS -M s_kaul01@uni-muenster.de
+#PBS -m ae
+#PBS -q default
+#PBS -N dunecontrol
+#PBS -j oe
+cd /scratch/tmp/s_kaul01/dune-ms-super
+export LD_LIBRARY_PATH=/Applic.PALMA/compiler/gcc/4.7.2/lib64:$LD_LIBRARY_PATH
+# export BOOST_ROOT=/Applic.PALMA/boost/boost_1_48_0
+# source BOOST_PATH.sh
+
+# this should build dune-common dune-geometry dune-grid dune-istl dune-localfunctions dune-fem dune-subgrid dune-stuff dune-spgrid dune-multiscale
+./dune-common/bin/dunecontrol --use-cmake --opts=config.opts.palma --only=dune-multiscale all
