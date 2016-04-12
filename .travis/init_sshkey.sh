e865058ec7ef9790618677631d19c44f938742c8
Rene Milk <rene.milk@wwu.de>
2016-04-12 15:37:30 +0200

    [travis] switchup ssh key init

--- .travis/init_sshkey.sh
+++ .travis/init_sshkey.sh
@@ -7,6 +7,10 @@ IV=${2}
 TESTLOGS=${TRAVIS_REPO_SLUG/\//_}-testlogs
 RSAKEY=${3:-TESTLOGS}.rsa.enc
 pushd ${DIR}
-openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
+if [ ${IV} == "none" ] ; then
+  openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
+else
+  openssl aes-256-cbc -K ${KEY} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
+fi
 chmod 600 ~/.ssh/id_rsa
 popd
