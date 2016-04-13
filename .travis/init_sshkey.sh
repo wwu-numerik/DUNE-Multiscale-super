494030e51f331208eb2e7183fb0843b526be4aa4
Rene Milk <rene.milk@wwu.de>
2016-04-13 13:30:38 +0200

    [travis] fix key check logic

--- .travis/init_sshkey.sh
+++ .travis/init_sshkey.sh
@@ -8,9 +8,9 @@ TESTLOGS=${TRAVIS_REPO_SLUG/\//_}-testlogs
 RSAKEY=${3:-TESTLOGS}.rsa.enc
 pushd ${DIR}
 if [ ${IV} == "none" ] ; then
-  openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
-else
   openssl aes-256-cbc -K ${KEY} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
+else
+  openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
 fi
 chmod 600 ~/.ssh/id_rsa
 popd
