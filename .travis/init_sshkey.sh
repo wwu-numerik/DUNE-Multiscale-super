0fb9ae7c8df4d9d6fe67750cf064da7482eec5f0
Rene Milk <rene.milk@wwu.de>
2016-04-13 14:20:31 +0200

    simplify key init again

--- .travis/init_sshkey.sh
+++ .travis/init_sshkey.sh
@@ -7,10 +7,6 @@ IV=${2}
 TESTLOGS=${TRAVIS_REPO_SLUG/\//_}-testlogs
 RSAKEY=${3:-TESTLOGS}.rsa.enc
 pushd ${DIR}
-if [ ${IV} == "none" ] ; then
-  openssl aes-256-cbc -K ${KEY} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
-else
-  openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
-fi
+openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 popd
