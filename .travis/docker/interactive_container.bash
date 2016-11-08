e9bc458249109556e7dc402b0d8f7a6242d2fdb4
Rene Milk <rene.milk@wwu.de>
2016-11-08 13:01:05 +0100

    enable docker infrastructure

---
+++ .travis/docker/interactive_container.bash
@@ -0,0 +1,5 @@
+DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd ../../ ; pwd -P )"
+
+docker run -it -v ${DIR}/dune-multiscale:/root/src/dune-multiscale \
+    -v ${DIR}/dune-mlmc:/root/src/dune-mlmc \
+    dunecommunity/dune-multiscale-testing:gcc-5_master
---
+++ .travis/docker/update_image.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+
+cc_mapping = {'gcc-5': 'g++-5', 'gcc-6': 'g++-6', 'clang-3.8': 'clang++-3.8', 'clang-3.9': 'clang++-3.9'}
+
+def update(branch, cc):
+    gdt_super_dir = os.path.join(os.path.dirname(__file__), '..', '..',)
+    dockerfile = os.path.join(os.path.dirname(__file__), 'dune-multiscale-testing.docker')
+
+    os.chdir(gdt_super_dir)
+
+    cxx = cc_mapping[cc]
+    subprocess.check_call(['docker', 'build', '-f', '.travis/docker/dune-multiscale-testing.docker',
+                        '-t', 'dunecommunity/dune-multiscale-testing:{}_{}'.format(cc, branch), '--build-arg', 'cc={}'.format(cc),
+                        '--build-arg', 'cxx={}'.format(cxx), '.'])
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        ccs = [sys.argv[1]]
+        branches = [sys.argv[2]]
+    else:
+        ccs = list(cc_mapping.keys())
+        branches = ['master']
+
+    for b in branches:
+        for c in ccs:
+            update(b, c)
+
+    subprocess.check_call(['docker', '--log-level="debug"', 'images'])
+    subprocess.check_call(['docker', '--log-level="debug"', 'push', 'dunecommunity/dune-multiscale-testing'])
