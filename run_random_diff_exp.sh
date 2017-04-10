072c1e567974ced2a04a5676d6e7e744406c6e6c
Nikolay Shegunov <shegunov@login1.beehive.itwm.fhg.de>
2017-04-10 15:40:13 +0200

    Removed some old files dune-random-diff is now submodule (typo fix from previous commit) clean successful build

--- run_random_diff_exp.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash!
-echo "Usage: <total num of p> , <p fast>, <p med.>, <p slow> <gird sz> <corr len> <sigma> <levels> [out file]"
-mpirun -np ${1} ./dune-random-diff-exp/build-release/src/dune-random-diff ${2} ${3} ${4} ${5} 1 ${6} ${7} 5e-3 3 ${8} ${9}
