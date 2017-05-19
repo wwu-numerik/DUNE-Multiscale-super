9f7594f4cef45e4dc2bc67a4a6cd6111d405ba40
Rene Milk <rene.milk@wwu.de>
2017-05-19 15:58:55 +0200

    update docker infrastructure

--- .travis/docker/interactive_container.bash
+++ .travis/docker/interactive_container.bash
@@ -2,4 +2,4 @@ DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd ../../ ; pwd -P )"
 
 docker run -it -v ${DIR}/dune-multiscale:/root/src/dune-multiscale \
     -v ${DIR}/dune-mlmc:/root/src/dune-mlmc \
-    dunecommunity/dune-multiscale-testing:gcc-5_master
+    dunecommunity/dune-multiscale-testing_gcc:master
--- .travis/docker/update_image.py
+++ .travis/docker/update_image.py
@@ -4,19 +4,22 @@ import os
 import subprocess
 import sys
 
-
-cc_mapping = {'gcc-5': 'g++-5', 'gcc-6': 'g++-6', 'clang-3.8': 'clang++-3.8', 'clang-3.9': 'clang++-3.9'}
+cc_mapping = {'gcc': 'g++', 'clang': 'clang++'}
 
 def update(branch, cc):
     gdt_super_dir = os.path.join(os.path.dirname(__file__), '..', '..',)
-    dockerfile = os.path.join(os.path.dirname(__file__), 'dune-multiscale-testing.docker')
-
     os.chdir(gdt_super_dir)
-
     cxx = cc_mapping[cc]
-    subprocess.check_call(['docker', 'build', '-f', '.travis/docker/dune-multiscale-testing.docker',
-                        '-t', 'dunecommunity/dune-multiscale-testing:{}_{}'.format(cc, branch), '--build-arg', 'cc={}'.format(cc),
-                        '--build-arg', 'cxx={}'.format(cxx), '.'])
+
+    #for sub in [ 'dune-multiscale-dev']:
+    for sub in ['dune-multiscale-testing', 'dune-multiscale-dev']:
+        dockerfile = os.path.join(os.path.dirname(__file__), sub, 'Dockerfile')
+        repo = 'dunecommunity/{}_{}'.format(sub, cc)
+        for tag in [branch, 'latest']:
+            subprocess.check_call(['docker', 'build', '-f', dockerfile,
+                                '-t', '{}:{}'.format(repo, tag), '--build-arg', 'cc={}'.format(cc),
+                                '--build-arg', 'cxx={}'.format(cxx), '.'])
+        subprocess.check_call(['docker', '--log-level="debug"', 'push', repo])
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
@@ -29,6 +32,3 @@ if __name__ == '__main__':
     for b in branches:
         for c in ccs:
             update(b, c)
-
-    subprocess.check_call(['docker', '--log-level="debug"', 'images'])
-    subprocess.check_call(['docker', '--log-level="debug"', 'push', 'dunecommunity/dune-multiscale-testing'])
